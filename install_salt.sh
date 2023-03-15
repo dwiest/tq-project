37b3d9890da554950df5cd17e7efd0a160f64e71
dwiest <dw9494@gmail.com>
2023-03-15 16:10:30 +0000

    Fixed interpreter typo.

--- install_salt.sh
+++ install_salt.sh
@@ -1,4 +1,4 @@
-#!/bin.sh
+#!/bin/sh
 sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/ubuntu/20.04/amd64/3004/salt-archive-keyring.gpg
 
 echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/ubuntu/20.04/amd64/3004 focal main" | sudo tee /etc/apt/sources.list.d/salt.list
